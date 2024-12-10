514f15a9fb6633f393a566e56b808417a494557d
CanerYesiltas <caneryesiltas1@gmail.com>
2024-12-09 20:27:43 -0500

    add toggle theme

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,57 @@
-import React from 'react';
-import '../styles/Header.css'
+import React, { useState } from 'react';
+import '../styles/Header.css';
+import { CiShoppingBasket } from 'react-icons/ci';
+import { CiLight } from 'react-icons/ci';
+import { FaMoon } from 'react-icons/fa';
 
 const Header = () => {
-  return <div style={{display:'flex', FlexDirection:'row', justifyContent:'space-between'   }}  >
-    <div className='flex-row'>
-    <img src="/logo.png" alt="logo" className="logo" />
-    <p className='logo-text'> Caner Corp. </p>
+  const [theme, setTheme] = useState(false);
+
+  const changeTheme = () => {
+    const root = document.getElementById('root');
+    setTheme(!theme);
+    if (theme) {
+      root.style.backgroundColor = 'black';
+      root.style.color = '#fff';
+    } else {
+      root.style.backgroundColor = '#fff';
+      root.style.color = 'black';
+    }
+   
+  };
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        FlexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingTop: '2rem',
+      }}
+    >
+      <div className='flex-row'>
+        <img src='/logo.png' alt='logo' className='logo' />
+        <p className='logo-text'> Caner Corp. </p>
+      </div>
+      <div className='flex-row'>
+        <input
+          className='search-input'
+          type='text'
+          placeholder='Search Something'
+        />
+
+        <div>
+          {theme ? (
+            <FaMoon className='icon' onClick={changeTheme}  />
+          ) : (
+            <CiLight className='icon' onClick={changeTheme} />
+          )}
+
+          <CiShoppingBasket className='icon' />
+        </div>
+      </div>
     </div>
-    <div>
-        <input type="text"  />
-    </div>
-  </div>
+  );
 };
 
 export default Header;
